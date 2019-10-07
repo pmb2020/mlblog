192de834538dc01d0d91956e07491bd6bacd928c
wangbo <pmb2020@163.com>
2019-10-07 23:56:15 +0800

    00

---
+++ application/index/controller/Comment.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use think\Request;
+
+/**
+ * 评论控制器
+ */
+class Comment extends Controller {
+
+	function index() {
+		// $data = input('post.');
+		// $data = json_decode($data);
+		// $res=db('ml_comment').insert($data);
+		// return gettype($data);
+		$asad = ['titlt', 'wwwwwwww'];
+		return 'wwww';
+	}
+}
